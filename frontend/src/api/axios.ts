59563ab88ba68bc3c3760ae9db176e4cc63690f7
Prajin Kumar P <23cs1554@stjosephstechnology.ac.in>
2026-04-16 20:01:49 +0530

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,16 +1,15 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
-
 const api = axios.create({
   baseURL: '/api',
 });
-
 api.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token;
+  const token = useAuthStore.getState().token;   
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+  
 });
 
 export default api;
